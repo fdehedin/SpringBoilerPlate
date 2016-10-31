7dda2a05061b47c11cdc07b6fab04e7f429eb0f7
Frédéric Dehédin <frederic.dehedin@belsoft.ch>
2016-10-31 13:56:15 +0100

    gitignore

---
+++ SpringBoilerPlateREST/src/main/java/ch/fdehedin/springconfig/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package ch.fdehedin.springconfig;
+
+import com.google.common.base.Predicate;
+import static com.google.common.base.Predicates.or;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.ApiInfoBuilder;
+import static springfox.documentation.builders.PathSelectors.regex;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).groupName("person").apiInfo(apiInfo()).select().paths(paths())
+				.build();
+	}
+
+	// Here is an example where we select any api that matches one of these
+	// paths
+	private Predicate<String> paths() {
+		return or(regex("/api/person.*"), regex("/api/*"));
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("SpringMVC BoilerPlate API")
+				.description("This is the API reference to my Spring BoilerPlate Project")
+				.termsOfServiceUrl("http://www.fdehedin.ch")
+				.contact(new Contact("Frédéric Dehédin", "http://www.fdehedin.ch", "info@fdehedin.ch"))
+				.license("Apache License Version 2.0").licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+				.version("2.0").build();
+	}
+
+}
---
+++ SpringBoilerPlateREST/src/test/java/ch/fdehedin/controller/PersonDaoTest.java
@@ -0,0 +1,61 @@
+package ch.fdehedin.controller;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import ch.fdehedin.model.Person;
+
+public class PersonDaoTest {
+
+	private static final Logger LOGGER = Logger.getLogger(PersonDaoTest.class.getName());
+
+	public static final String REST_SERVICE_URI = "http://localhost:8080/SpringBoilerPlate/api";
+
+	/* GET */
+	@SuppressWarnings("unchecked")
+	@Test
+	public void getAll() {
+		try {
+
+			final RestTemplate restTemplate = new RestTemplate();
+			ParameterizedTypeReference<List<Person>> responseType = new ParameterizedTypeReference<List<Person>>() {
+			};
+
+			final String url = REST_SERVICE_URI + "/person/all";
+			LOGGER.log(Level.INFO, "getAll, url: " + url);
+			//final ResponseEntity<? extends ArrayList<Person>> responseEntity = restTemplate.getForEntity(url,
+			//		(Class<? extends ArrayList<Person>>) ArrayList.class);
+
+			final ResponseEntity<List<Person>> responseEntity = restTemplate.exchange(url, HttpMethod.GET, null,
+					responseType);
+
+			final HttpStatus statusCode = responseEntity.getStatusCode();
+			Assert.assertEquals(HttpStatus.OK.toString(), statusCode.toString());
+
+			assertThat(responseEntity.hasBody(), is(true));
+
+			final List<Person> lst = responseEntity.getBody();
+			assertThat(lst.isEmpty(), is(false));
+
+			LOGGER.log(Level.INFO, "getAll, count: " + lst.size());
+
+		} catch (final Exception e) {
+			LOGGER.log(Level.SEVERE, e.toString(), e);
+			Assert.fail(e.getMessage());
+
+		}
+	}
+
+}
